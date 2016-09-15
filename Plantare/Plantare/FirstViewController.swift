7dba4becd19b0efe40bc83cc51a33021347ca9c2
Gabriel Bernardo Pereira <gabeira@gmail.com>
2016-09-16 02:23:54 +1000

    First Commit

---
+++ Plantare/Plantare/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Plantare
+//
+//  Created by Gabriel Bernardo Pereira on 9/16/16.
+//  Copyright © 2016 Gabriel Bernardo Pereira. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
